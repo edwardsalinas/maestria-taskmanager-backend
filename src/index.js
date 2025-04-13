bad13a5a7d3601ef2763613d7b536bed8149769a
Edward Salinas <edwsalr.es@gmail.com>
2025-04-13 04:51:38 -0400

    add cors

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://maestria-taskmanager-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Rutas públicas
